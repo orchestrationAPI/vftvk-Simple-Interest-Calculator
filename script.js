b2dcfefe22138327345618c8f67e60081eaaef63
radvel26 <radvel26@in.ibm.com>
2021-02-16 16:24:01 +0000

    Web App – Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+function sliderChange(val) {
+document.getElementById('sliderVal').innerHTML = val;
+
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var yearsVal = document.getElementById("years").value;
+    if ( principal <= 0 ) {
+        alert("Enter Positive number");
+        return false;
+    } else{
+
+    var interest=principal*yearsVal*rate/100;
+     var d = new Date();
+     var currentyear = d.getFullYear();
+    var totalDays = yearsVal * 365;
+    var years = Math.floor(currentyear) + Math.floor(totalDays/365);
+      
+    document.getElementById('result').innerHTML  = "If you deposit <mark> "+ principal+",</mark><br/> at an interest rate of <mark>"+ rate +"% .</mark><br/>You will receive an amount of <mark>"+ interest+",</mark><br/>in the year <mark>"+ years +"</mark>";
+      Logger.log(result);          
+
+    }
     
 }
-        
